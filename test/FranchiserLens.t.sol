dced5589ae3cf70c8bbc946b4a468712ad0b729d
Noah Zinsmeister <noahwz@gmail.com>
2022-07-01 14:28:30 -0400

    improve lens tests

--- test/FranchiserLens.t.sol
+++ test/FranchiserLens.t.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.15;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {VotingTokenConcrete} from "./VotingTokenConcrete.sol";
 import {FranchiserFactory} from "../src/FranchiserFactory.sol";
 import {FranchiserLens} from "../src/FranchiserLens.sol";
 import {IVotingToken} from "../src/interfaces/IVotingToken.sol";
 import {Franchiser} from "../src/Franchiser.sol";
 import {IFranchiserLens} from "../src/interfaces/IFranchiserLens.sol";
+import {Utils} from "./Utils.sol";
 
 contract FranchiserLensTest is Test {
     address private constant alice = 0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa;
@@ -39,127 +40,111 @@ contract FranchiserLensTest is Test {
         );
     }
 
-    function testGetRootDelegationNoNesting() public {
-        vm.prank(alice);
-        Franchiser franchiser = franchiserFactory.fund(bob, 0);
-        IFranchiserLens.Delegation memory delegation = franchiserLens
-            .getRootDelegation(franchiser);
-        assertEq(delegation.delegator, alice);
-        assertEq(delegation.delegatee, bob);
-        assertEq(address(delegation.franchiser), address(franchiser));
-    }
+    function testNesting1() public {
+        Franchiser[5] memory franchisers = Utils.nestVertical(
+            1,
+            vm,
+            votingToken,
+            franchiserFactory
+        );
 
-    function testGetRootDelegationNesting() public {
-        vm.prank(alice);
-        Franchiser bobFranchiser = franchiserFactory.fund(bob, 0);
-        vm.prank(bob);
-        Franchiser carolFranchiser = bobFranchiser.subDelegate(carol, 0);
-        IFranchiserLens.Delegation memory delegation = franchiserLens
-            .getRootDelegation(carolFranchiser);
+        IFranchiserLens.Delegation memory rootDelegation = franchiserLens
+            .getRootDelegation(franchisers[0]);
+        assertEq(rootDelegation.delegator, alice);
+        assertEq(rootDelegation.delegatee, bob);
+        assertEq(address(rootDelegation.franchiser), address(franchisers[0]));
 
-        assertEq(delegation.delegator, alice);
-        assertEq(delegation.delegatee, bob);
-        assertEq(address(delegation.franchiser), address(bobFranchiser));
-    }
-
-    function nestMaximumVertical()
-        private
-        returns (Franchiser[] memory franchisers)
-    {
-        franchisers = new Franchiser[](5);
-        vm.prank(alice);
-        franchisers[0] = franchiserFactory.fund(bob, 0);
-        vm.prank(bob);
-        franchisers[1] = franchisers[0].subDelegate(carol, 0);
-        vm.prank(carol);
-        franchisers[2] = franchisers[1].subDelegate(dave, 0);
-        vm.prank(dave);
-        franchisers[3] = franchisers[2].subDelegate(erin, 0);
-        vm.prank(erin);
-        franchisers[4] = franchisers[3].subDelegate(frank, 0);
-    }
-
-    function testGetRootDelegationMaximumNesting() public {
-        Franchiser[] memory franchisers = nestMaximumVertical();
-        IFranchiserLens.Delegation memory delegation = franchiserLens
-            .getRootDelegation(franchisers[franchisers.length - 1]);
-
-        assertEq(delegation.delegator, alice);
-        assertEq(delegation.delegatee, bob);
-        assertEq(address(delegation.franchiser), address(franchisers[0]));
-    }
-
-    function testGetVerticalDelegationsNoNesting() public {
-        vm.prank(alice);
-        Franchiser franchiser = franchiserFactory.fund(bob, 0);
-        IFranchiserLens.Delegation[]
-            memory delegations = new IFranchiserLens.Delegation[](1);
-        delegations[0] = IFranchiserLens.Delegation({
-            delegator: alice,
-            delegatee: bob,
-            franchiser: franchiser
-        });
+        IFranchiserLens.Delegation[] memory verticalDelegations = franchiserLens
+            .getVerticalDelegations(franchisers[0]);
+        assertEq(verticalDelegations.length, 1);
         assertEq(
-            keccak256(
-                abi.encode(franchiserLens.getVerticalDelegations(franchiser))
-            ),
-            keccak256(abi.encode(delegations))
+            keccak256(abi.encode(verticalDelegations[0])),
+            keccak256(abi.encode(rootDelegation))
+        );
+
+        IFranchiserLens.Delegation[]
+            memory horizontalDelegations = franchiserLens
+                .getHorizontalDelegations(franchisers[0]);
+        assertEq(horizontalDelegations.length, 0);
+    }
+
+    function testNesting2() public {
+        Franchiser[5] memory franchisers = Utils.nestVertical(
+            2,
+            vm,
+            votingToken,
+            franchiserFactory
+        );
+
+        IFranchiserLens.Delegation memory rootDelegation = franchiserLens
+            .getRootDelegation(franchisers[0]);
+        assertEq(rootDelegation.delegator, alice);
+        assertEq(rootDelegation.delegatee, bob);
+        assertEq(address(rootDelegation.franchiser), address(franchisers[0]));
+
+        IFranchiserLens.Delegation[] memory verticalDelegations = franchiserLens
+            .getVerticalDelegations(franchisers[1]);
+        assertEq(verticalDelegations.length, 2);
+        assertEq(verticalDelegations[0].delegator, bob);
+        assertEq(verticalDelegations[0].delegatee, carol);
+        assertEq(
+            address(verticalDelegations[0].franchiser),
+            address(franchisers[1])
+        );
+        assertEq(
+            keccak256(abi.encode(verticalDelegations[1])),
+            keccak256(abi.encode(rootDelegation))
+        );
+
+        IFranchiserLens.Delegation[]
+            memory horizontalDelegations = franchiserLens
+                .getHorizontalDelegations(franchisers[0]);
+        assertEq(horizontalDelegations.length, 1);
+        assertEq(horizontalDelegations[0].delegator, bob);
+        assertEq(horizontalDelegations[0].delegatee, carol);
+        assertEq(
+            address(horizontalDelegations[0].franchiser),
+            address(franchisers[1])
         );
     }
 
-    function testGetVerticalDelegationsNesting() public {
-        vm.prank(alice);
-        Franchiser bobFranchiser = franchiserFactory.fund(bob, 0);
-        vm.prank(bob);
-        Franchiser carolFranchiser = bobFranchiser.subDelegate(carol, 0);
-        IFranchiserLens.Delegation[]
-            memory delegations = new IFranchiserLens.Delegation[](2);
-        delegations[0] = IFranchiserLens.Delegation({
-            delegator: bob,
-            delegatee: carol,
-            franchiser: carolFranchiser
-        });
-        delegations[1] = IFranchiserLens.Delegation({
-            delegator: alice,
-            delegatee: bob,
-            franchiser: bobFranchiser
-        });
-        assertEq(
-            keccak256(
-                abi.encode(
-                    franchiserLens.getVerticalDelegations(carolFranchiser)
-                )
-            ),
-            keccak256(abi.encode(delegations))
+    function testNesting5() public {
+        Franchiser[5] memory franchisers = Utils.nestVertical(
+            5,
+            vm,
+            votingToken,
+            franchiserFactory
         );
-    }
 
-    function testGetVerticalDelegationsMaximumNesting() public {
-        Franchiser[] memory franchisers = nestMaximumVertical();
+        IFranchiserLens.Delegation memory rootDelegation = franchiserLens
+            .getRootDelegation(franchisers[0]);
+        assertEq(rootDelegation.delegator, alice);
+        assertEq(rootDelegation.delegatee, bob);
+        assertEq(address(rootDelegation.franchiser), address(franchisers[0]));
 
         IFranchiserLens.Delegation[]
-            memory delegations = new IFranchiserLens.Delegation[](5);
-        delegations[0] = IFranchiserLens.Delegation({
+            memory verticalDelegations = new IFranchiserLens.Delegation[](5);
+        verticalDelegations[0] = IFranchiserLens.Delegation({
             delegator: erin,
             delegatee: frank,
             franchiser: franchisers[4]
         });
-        delegations[1] = IFranchiserLens.Delegation({
+        verticalDelegations[1] = IFranchiserLens.Delegation({
             delegator: dave,
             delegatee: erin,
             franchiser: franchisers[3]
         });
-        delegations[2] = IFranchiserLens.Delegation({
+        verticalDelegations[2] = IFranchiserLens.Delegation({
             delegator: carol,
             delegatee: dave,
             franchiser: franchisers[2]
         });
-        delegations[3] = IFranchiserLens.Delegation({
+        verticalDelegations[3] = IFranchiserLens.Delegation({
             delegator: bob,
             delegatee: carol,
             franchiser: franchisers[1]
         });
-        delegations[4] = IFranchiserLens.Delegation({
+        verticalDelegations[4] = IFranchiserLens.Delegation({
             delegator: alice,
             delegatee: bob,
             franchiser: franchisers[0]
@@ -172,67 +157,29 @@ contract FranchiserLensTest is Test {
                     )
                 )
             ),
-            keccak256(abi.encode(delegations))
+            keccak256(abi.encode(verticalDelegations))
         );
+
+        unchecked {
+            for (uint256 i; i < 4; i++) {
+                IFranchiserLens.Delegation[]
+                    memory horizontalDelegations = franchiserLens
+                        .getHorizontalDelegations(franchisers[i]);
+                assertEq(horizontalDelegations.length, 1);
+                assertEq(
+                    address(horizontalDelegations[0].franchiser),
+                    address(franchisers[i + 1])
+                );
+            }
+        }
     }
 
     function testGetAllDelegations() public {
-        votingToken.mint(alice, 64);
-
-        Franchiser[][5] memory franchisers;
-        franchisers[0] = new Franchiser[](1);
-        franchisers[1] = new Franchiser[](8);
-        franchisers[2] = new Franchiser[](8 * 4);
-        franchisers[3] = new Franchiser[](8 * 4 * 2);
-        franchisers[4] = new Franchiser[](8 * 4 * 2);
-
-        vm.startPrank(alice);
-        votingToken.approve(address(franchiserFactory), 64);
-        franchisers[0][0] = franchiserFactory.fund(bob, 64);
-        vm.stopPrank();
-
-        address nextDelegatee = address(1);
-        unchecked {
-            for (uint256 i; i < 8; i++) {
-                vm.prank(bob);
-                franchisers[1][i] = franchisers[0][0].subDelegate(
-                    nextDelegatee,
-                    8
-                );
-                nextDelegatee = address(uint160(nextDelegatee) + 1);
-            }
-            for (uint256 i; i < 8 * 4; i++) {
-                uint256 j = i / 4;
-                address delegator = franchisers[1][j].delegatee();
-                vm.prank(delegator);
-                franchisers[2][i] = franchisers[1][j].subDelegate(
-                    nextDelegatee,
-                    8 / 4
-                );
-                nextDelegatee = address(uint160(nextDelegatee) + 1);
-            }
-            for (uint256 i; i < 8 * 4 * 2; i++) {
-                uint256 j = i / 2;
-                address delegator = franchisers[2][j].delegatee();
-                vm.prank(delegator);
-                franchisers[3][i] = franchisers[2][j].subDelegate(
-                    nextDelegatee,
-                    8 / 4 / 2
-                );
-                nextDelegatee = address(uint160(nextDelegatee) + 1);
-            }
-            for (uint256 i; i < 8 * 4 * 2; i++) {
-                address delegator = franchisers[3][i].delegatee();
-                vm.prank(delegator);
-                franchisers[4][i] = franchisers[3][i].subDelegate(
-                    nextDelegatee,
-                    8 / 4 / 2
-                );
-                nextDelegatee = address(uint160(nextDelegatee) + 1);
-            }
-        }
-
-        assertEq(uint160(nextDelegatee), 1 + 8 + 32 + 64 * 2);
+        Franchiser[][5] memory franchisers = Utils.nestMaximum(
+            vm,
+            votingToken,
+            franchiserFactory
+        );
 
         IFranchiserLens.DelegationWithVotes[][]
             memory delegationsWithVotes = franchiserLens.getAllDelegations(
@@ -241,22 +188,22 @@ contract FranchiserLensTest is Test {
         assertEq(delegationsWithVotes.length, 5);
         assertEq(delegationsWithVotes[0].length, 1);
         assertEq(delegationsWithVotes[1].length, 8);
-        assertEq(delegationsWithVotes[2].length, 8 * 4);
-        assertEq(delegationsWithVotes[3].length, 8 * 4 * 2);
-        assertEq(delegationsWithVotes[4].length, 8 * 4 * 2);
+        assertEq(delegationsWithVotes[2].length, 32);
+        assertEq(delegationsWithVotes[3].length, 64);
+        assertEq(delegationsWithVotes[4].length, 64);
 
-        assertEq(delegationsWithVotes[0][0].delegator, alice);
-        assertEq(delegationsWithVotes[0][0].delegatee, bob);
-        assertEq(
-            address(delegationsWithVotes[0][0].franchiser),
-            address(franchisers[0][0])
-        );
-        assertEq(delegationsWithVotes[0][0].votes, 0);
+        assertEq(delegationsWithVotes[0][0].delegator, address(1));
+        assertEq(delegationsWithVotes[0][0].delegatee, address(2));
 
         unchecked {
             for (uint256 i; i < delegationsWithVotes.length; i++)
-                for (uint256 j; j < delegationsWithVotes[i].length; j++)
+                for (uint256 j; j < delegationsWithVotes[i].length; j++) {
+                    assertEq(
+                        address(delegationsWithVotes[i][j].franchiser),
+                        address(franchisers[i][j])
+                    );
                     assertEq(delegationsWithVotes[i][j].votes, i == 4 ? 1 : 0);
+                }
         }
     }
 }
---
+++ test/Utils.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import {Vm} from "forge-std/Vm.sol";
+import {VotingTokenConcrete} from "./VotingTokenConcrete.sol";
+import {FranchiserFactory} from "../src/FranchiserFactory.sol";
+import {Franchiser} from "../src/Franchiser.sol";
+
+library Utils {
+    address private constant alice = 0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa;
+    address private constant bob = 0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB;
+    address private constant carol = 0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC;
+    address private constant dave = 0xDDdDddDdDdddDDddDDddDDDDdDdDDdDDdDDDDDDd;
+    address private constant erin = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address private constant frank = 0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF;
+
+    function nestVertical(
+        uint256 levels,
+        Vm vm,
+        VotingTokenConcrete votingToken,
+        FranchiserFactory franchiserFactory
+    ) internal returns (Franchiser[5] memory franchisers) {
+        assert(levels != 0);
+        assert(levels <= 5);
+        assert(franchiserFactory.initialMaximumSubDelegatees() == 8);
+        assert(franchiserFactory.franchiserImplementation().decayFactor() == 2);
+
+        address[5] memory delegatees = [bob, carol, dave, erin, frank];
+
+        votingToken.mint(alice, 1);
+        vm.startPrank(alice);
+        votingToken.approve(address(franchiserFactory), 1);
+        franchisers[0] = franchiserFactory.fund(delegatees[0], 1);
+        vm.stopPrank();
+
+        unchecked {
+            for (uint256 i = 1; i < levels; i++) {
+                vm.prank(delegatees[i - 1]);
+                franchisers[i] = franchisers[i - 1].subDelegate(
+                    delegatees[i],
+                    1
+                );
+            }
+        }
+    }
+
+    function incrementNextDelegatee(address nextDelegatee)
+        private
+        pure
+        returns (address)
+    {
+        return address(uint160(nextDelegatee) + 1);
+    }
+
+    function nestMaximum(
+        Vm vm,
+        VotingTokenConcrete votingToken,
+        FranchiserFactory franchiserFactory
+    ) internal returns (Franchiser[][5] memory franchisers) {
+        assert(franchiserFactory.initialMaximumSubDelegatees() == 8);
+        assert(franchiserFactory.franchiserImplementation().decayFactor() == 2);
+
+        franchisers[0] = new Franchiser[](1);
+        franchisers[1] = new Franchiser[](8);
+        franchisers[2] = new Franchiser[](32);
+        franchisers[3] = new Franchiser[](64);
+        franchisers[4] = new Franchiser[](64);
+
+        address nextDelegatee = address(2);
+
+        votingToken.mint(address(1), 64);
+        vm.startPrank(address(1));
+        votingToken.approve(address(franchiserFactory), 64);
+        franchisers[0][0] = franchiserFactory.fund(nextDelegatee, 64);
+        vm.stopPrank();
+
+        nextDelegatee = incrementNextDelegatee(nextDelegatee);
+
+        unchecked {
+            for (uint256 i; i < 8; i++) {
+                address delegator = franchisers[0][0].delegatee();
+                vm.prank(delegator);
+                franchisers[1][i] = franchisers[0][0].subDelegate(
+                    nextDelegatee,
+                    8
+                );
+                nextDelegatee = address(uint160(nextDelegatee) + 1);
+            }
+            for (uint256 i; i < 32; i++) {
+                uint256 j = i / 4;
+                address delegator = franchisers[1][j].delegatee();
+                vm.prank(delegator);
+                franchisers[2][i] = franchisers[1][j].subDelegate(
+                    nextDelegatee,
+                    2
+                );
+                nextDelegatee = address(uint160(nextDelegatee) + 1);
+            }
+            for (uint256 i; i < 64; i++) {
+                uint256 j = i / 2;
+                address delegator = franchisers[2][j].delegatee();
+                vm.prank(delegator);
+                franchisers[3][i] = franchisers[2][j].subDelegate(
+                    nextDelegatee,
+                    1
+                );
+                nextDelegatee = address(uint160(nextDelegatee) + 1);
+            }
+            for (uint256 i; i < 64; i++) {
+                address delegator = franchisers[3][i].delegatee();
+                vm.prank(delegator);
+                franchisers[4][i] = franchisers[3][i].subDelegate(
+                    nextDelegatee,
+                    1
+                );
+                nextDelegatee = address(uint160(nextDelegatee) + 1);
+            }
+        }
+        assert(uint160(nextDelegatee) == 171);
+    }
+}
